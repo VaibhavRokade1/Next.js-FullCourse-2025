7925efe1e0a12f902590a986aa160a0fe50253b5
VaibhavRokade1 <rokadevaibhv04@gmail.com>
2025-11-02 16:59:11 +0530

    next js react route

---
+++ 03next-routing/src/app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body cz-shortcut-listen="true">{children}</body>
+    </html>
+  );
+}
---
+++ 03next-routing/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Next.js Routing Example</h1>
+    </main>
+  );
+}
