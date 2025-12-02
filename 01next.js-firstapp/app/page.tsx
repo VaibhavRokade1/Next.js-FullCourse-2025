12de822864b57d8498d7cc9e797cd74f954787de
VaibhavRokade1 <rokadevaibhv04@gmail.com>
2025-12-02 18:06:40 +0530

    changes draft

--- 01next.js-firstapp/app/page.tsx
+++ 01next.js-firstapp/app/page.tsx
@@ -1,7 +1,41 @@
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hello World</h1>
+    <div className="bg-gray-200 min-h-screen p-4">
+      <h1 className="text-center my-5 text-4xl font-bold">Register Form</h1>
+      <form className="shadow-md border rounded-2xl p-6 md:w-1/2 lg:w-1/3 mx-auto">
+        <div className="mb-4">
+          <div className="mb-1 font-semibold">First Name</div>
+          <input type="text" className="w-full border rounded-sm p-2" />
+        </div>
+
+        <div className="mb-4">
+          <div className="mb-1 font-semibold">Last Name</div>
+          <input type="text" className="w-full border rounded-sm p-2" />
+        </div>
+
+        <div className="mb-4">
+          <div className="mb-1 font-semibold">Email</div>
+          <input type="email" className="w-full border rounded-sm p-2" />
+        </div>
+
+        <div className="mb-4">
+          <div className="mb-1 font-semibold">Password</div>
+          <input type="password" className="w-full border rounded-sm p-2" />
+        </div>
+
+        <div className="mb-4">
+          <div className="mb-1 font-semibold">Confirm Password</div>
+          <input type="password" className="w-full border rounded-sm p-2" />
+        </div>
+        <div className="mb-4">
+          <button
+            type="submit"
+            className="w-full py-2 rounded-sm font-semibold bg-green-400 text-white"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
