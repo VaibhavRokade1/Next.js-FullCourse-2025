9007c32741332cc6c897b9a5b71bffc45f301a03
VaibhavRokade1 <rokadevaibhv04@gmail.com>
2026-01-18 22:18:20 +0530

    evrithing is upto date

--- 08musicstudioproject/src/app/page.tsx
+++ 08musicstudioproject/src/app/page.tsx
@@ -1,65 +1,39 @@
-import Image from "next/image";
+"use client";
+import FeaturedCourses from "@/components/modules/FeaturedCourses";
+import { Button } from "@/components/ui/moving-border";
+import { Spotlight } from "@/components/ui/SpotLight";
+import Link from "next/link";
 
-export default function Home() {
+export default function HeroSection() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <>
+      <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <Spotlight
+          className="-top-40 left-0 md:-top-20 md:left-60"
+          fill="#fff"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+        <div className="p-4 relative z-10 w-full text-center">
+          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+            Master the art of music
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+            Dive into our comprehensive music courses and transform your musical
+            journey today. Whether you're a beginner or looking to refine your
+            skills, join us to unlock your true potential.
           </p>
+          <div className="mt-4">
+            <Link href={"/courses"}>
+              <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              >
+                Explore courses
+              </Button>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+      <FeaturedCourses />
+    </>
   );
 }
